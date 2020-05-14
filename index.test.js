8c9947a0df362b0f48ec91df84134513366b4686
Javier Fernandes <javier.fernandes@gmail.com>
2020-05-14 08:48:12 -0300

    adding initial pretty simple example

---
+++ index.test.js
@@ -0,0 +1,11 @@
+
+const doIt = require('./index')
+
+describe('doIt test', () => {
+
+  it('shoud wait for the given amount of time and produce a message', async () => {
+    const r = await doIt(1234)
+    expect(r).toEqual('And we are done !')
+  })
+
+})
