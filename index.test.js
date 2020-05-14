1b0e1703885aa68ae0cfded472a939d460fa3e2a
Javier Fernandes <javier.fernandes@gmail.com>
2020-05-14 13:25:44 -0300

    forcing action to build

--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@ const doIt = require('./index')
 describe('doIt test', () => {
 
   it('shoud wait for the given amount of time and produce a message', async () => {
-    const r = await doIt(1250)
+    const r = await doIt(1251)
     expect(r).toEqual('And we are done !')
   })
 
