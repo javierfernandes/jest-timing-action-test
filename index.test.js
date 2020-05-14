b0c187b50bc52fa1f56da0a85dba8f4a5f1f9da9
Javier Fernandes <javier.fernandes@gmail.com>
2020-05-14 20:28:44 -0300

    adding more tests

--- index.test.js
+++ index.test.js
@@ -4,7 +4,17 @@ const doIt = require('./index')
 describe('doIt test', () => {
 
   it('shoud wait for the given amount of time and produce a message', async () => {
-    const r = await doIt(1234)
+    const r = await doIt(1200)
+    expect(r).toEqual('And we are done !')
+  })
+
+  it('a second test', async () => {
+    const r = await doIt(300)
+    expect(r).toEqual('And we are done !')
+  })
+
+  it('a third test', async () => {
+    const r = await doIt(150)
     expect(r).toEqual('And we are done !')
   })
 
---
+++ some/more/tests/good.test.js
@@ -0,0 +1,26 @@
+
+const doIt = require('../../../indexindex')
+
+describe('It is always good', () => {
+
+  it('to test', async () => {
+    const r = await doIt(1200)
+    expect(r).toEqual('And we are done !')
+  })
+
+  it('to test pure functions', async () => {
+    const r = await doIt(300)
+    expect(r).toEqual('And we are done !')
+  })
+
+  it('tests integrations', async () => {
+    const r = await doIt(150)
+    expect(r).toEqual('And we are done !')
+  })
+
+  it('but specially test side-effects!', async () => {
+    const r = await doIt(150)
+    expect(r).toEqual('And we are done !')
+  })
+
+})
